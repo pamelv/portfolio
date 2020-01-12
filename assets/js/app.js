4bb96ea9b49d3e4aa6527a364b707a4907e631de
Pamela Vasquez <pamelavasquez94@gmail.com>
2020-01-11 23:29:34 -0500

    DOCS: updated index file with main content and added style to page

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+$(".pg2").click(function() {
+  $(".hero-image").css("display", "none");
+  $(".animojileft").css("display", "flex");
+  $(".page2").css("display", "block");
+  $(".pg2").toggleClass("active");
+  $(".pg1").toggleClass("active");
+});
+$(".pg1").click(function() {
+  $(".hero-image").css("display", "block");
+  $(".animojileft").css("display", "none");
+  $(".page2").css("display", "none");
+  $(".pg1").toggleClass("active");
+  $(".pg2").toggleClass("active");
+});
